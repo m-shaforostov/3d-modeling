6577383a1bc7fb8fadc3a56f82690de004942477
m-shaforostov <maxim.shaforostov@gmail.com>
2023-07-25 23:28:08 +0300

    header content lg and md

--- src/header-block.js
+++ src/header-block.js
@@ -28,33 +28,49 @@ function HeaderBlock() {
                 </div>
                 <MenuBlock menuIsActive={menuIsActive} setMenuIsActive={setMenuIsActive}/>
             </div>
-            <div className="header-content flex w-full h-full justify-between">
-                {/*<div className="header-image">*/}
-                {/*    <img className="absolute" src={headerNet} alt=""/>*/}
-                {/*    <img className="absolute" src={headerLogo} alt=""/>*/}
-                {/*</div>*/}
-                {/*<div className="header-text">*/}
-                {/*    <div className="header-main-text">*/}
-                {/*        <p className="">modeling</p>*/}
-                {/*        <p className="">rendering</p>*/}
-                {/*        <p className="">animation</p>*/}
-                {/*    </div>*/}
-                {/*    <div className="header-additional-text">*/}
-                {/*        <ul>*/}
-                {/*            <li>Modeling and product visualization</li>*/}
-                {/*            <li>Photorealistic package visualisation</li>*/}
-                {/*            <li>CGI animation for presentations and ads</li>*/}
-                {/*        </ul>*/}
-                {/*    </div>*/}
-                {/*    <div className="header-try-button">*/}
-                {/*        <div className="button-shadow">*/}
+            <div className="header-content mt-16 flex w-full h-full justify-between">
+                <div className="header-image h-full">
+                    <img className="" src={headerNet} alt=""/>
+                    <img className="lg:-mt-96 md:-mt-72" src={headerLogo} alt=""/>
+                </div>
+                <div className="header-tex mt-6 flex flex-col items-end ml-14">
+                    <div className="header-main-text text-left uppercase md:text-5xl lg:text-7xl">
+                        <p className="header-bold-text ">modeling</p>
+                        <p className="header-normal-text">rendering</p>
+                        <p className="header-bold-text">animation</p>
+                    </div>
+                    <div className="header-additional-text lg:mt-12 md:mt-6 lg:text-lg md:text-base">
+                        <div className="flex flex-nowrap items-center justify-end mb-4">
+                            <p>Modeling and product visualization</p>
+                            <svg className="ml-2.5 mr-1.5" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M6 12C5.988 8.69037 3.30633 6.01112 0 6.01112C3.31371 6.01112 6 3.31982 6 0C6.01195 3.30963 8.69367 5.98888 12 5.98888C8.68624 5.98888 6 8.68018 6 12Z" fill="#303030"/>
+                            </svg>
+                        </div>
+                        <div className="flex flex-nowrap items-center justify-end mb-4">
+                            <p>Photorealistic package visualisation</p>
+                            <svg className="ml-2.5 mr-1.5" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M6 12C5.988 8.69037 3.30633 6.01112 0 6.01112C3.31371 6.01112 6 3.31982 6 0C6.01195 3.30963 8.69367 5.98888 12 5.98888C8.68624 5.98888 6 8.68018 6 12Z" fill="#303030"/>
+                            </svg>
+                        </div>
+                        <div className="flex flex-nowrap items-center justify-end whitespace-nowrap">
+                            <p>CGI animation for presentations and ads</p>
+                            <svg className="ml-2.5 mr-1.5" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M6 12C5.988 8.69037 3.30633 6.01112 0 6.01112C3.31371 6.01112 6 3.31982 6 0C6.01195 3.30963 8.69367 5.98888 12 5.98888C8.68624 5.98888 6 8.68018 6 12Z" fill="#303030"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="header-try-button lg:mt-14 md:mt-10 text-sm">
+                        <div className="button-shadow absolute -m-2.5 h-16 w-60 border-gen-grey border-solid border opacity-50">
 
-                {/*        </div>*/}
-                {/*        <div className="button">*/}
-                {/*            <p>Get in touch today {`U+2192`}</p>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                        </div>
+                        <div className="button flex bg-gen-grey h-16 w-60 items-center justify-center">
+                            <p className="mr-2.5">Get in touch today</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="9" viewBox="0 0 14 9" fill="none">
+                                <path d="M0.000366568 5.56264L0.000366686 4.41694L10.5481 4.41694L7.82863 2.12553L8.5085 0.979826L13.2676 4.98979L8.5085 8.99976L7.82863 7.85405L10.5481 5.56264L0.000366568 5.56264Z" fill="white"/>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/menuBlock.js
+++ src/menuBlock.js
@@ -1,11 +1,10 @@
 import './menuBlock.css';
-import menuIcon from "./images/menu.svg";
 import closeIcon from "./images/close.svg";
 
 function MenuBlock({menuIsActive,setMenuIsActive}) {
     return (
         menuIsActive &&
-        <div className="menu-block-wrapper flex px690:hidden absolute left-0 top-0 flex-col w-full text-white px-2 pb-2">
+        <div className="menu-block-wrapper flex px690:hidden absolute left-0 top-0 flex-col w-full h-48 text-white px-2 pb-2">
             <div className="header flex w-full items-center justify-between">
                 <div className="header-logo flex h-7 w-32 items-center flex-row justify-between flex-nowrap hover:cursor-pointer">
                     <p className="font-bold text-xl uppercase">Product</p>
@@ -16,9 +15,9 @@ function MenuBlock({menuIsActive,setMenuIsActive}) {
                 </div>
             </div>
             <div className="menu-content flex w-full flex-col text-right">
-                <div className="menu-item hover:cursor-pointer hover:bg-neutral-700 uppercase px-1">Our work</div>
-                <div className="menu-item hover:cursor-pointer hover:bg-neutral-700 uppercase px-1">Services and Prices</div>
-                <div className="menu-item hover:cursor-pointer hover:bg-neutral-700 uppercase px-1">Contacts</div>
+                <div className="menu-item hover:cursor-pointer hover:bg-neutral-700 uppercase px-1 mb-2">Our work</div>
+                <div className="menu-item hover:cursor-pointer hover:bg-neutral-700 uppercase px-1 mb-2">Services and Prices</div>
+                <div className="menu-item hover:cursor-pointer hover:bg-neutral-700 uppercase px-1 mb-2">Contacts</div>
             </div>
         </div>
     );
